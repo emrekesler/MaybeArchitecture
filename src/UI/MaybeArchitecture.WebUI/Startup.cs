2075afac4f15f3fefe8506c365000729a0f67421
Emre Kesler <emrekesler@outlook.com>
2021-02-17 14:50:28 +0300

    Trigger Commit for Docker Hub

--- src/UI/MaybeArchitecture.WebUI/Startup.cs
+++ src/UI/MaybeArchitecture.WebUI/Startup.cs
@@ -110,7 +110,6 @@ namespace MaybeArchitecture.WebUI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
